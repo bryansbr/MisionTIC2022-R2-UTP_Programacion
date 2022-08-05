097b28f66c2c2b537f2f797f73228361f92a414a
bryansbr <bryan.biojo@correounivalle.edu.co>
2022-08-05 16:34:31 -0500

    Nombre de paquete modificado en reto 4 ciclo 2.

--- Ciclo 2/Reto 4/Reto4_Construccion/src/reto4/app/App.java
+++ Ciclo 2/Reto 4/Reto4_Construccion/src/reto4/app/App.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package reto4.App;
+package reto4.app;
 
 import reto4.view.ReportesView;
 import java.sql.SQLException;
